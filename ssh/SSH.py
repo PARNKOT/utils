a68b1fed1129475fb102975d2b999c4363cb77dd
Parnkot <100835002+PARNKOT@users.noreply.github.com>
2022-12-29 13:46:17 +0300

    Add files via upload

---
+++ ssh/SSH.py
@@ -0,0 +1,63 @@
+import re
+import time
+
+
+class SSH:
+    def __init__(self, ssh_addr: str, password=''):
+        self.ssh_addr = ssh_addr
+        self.user, self.addr = ssh_addr.split('@')
+        self.__password = password
+        self.__client = paramiko.SSHClient()
+        self.__channel = None
+        self.last_command_exit_status: int = 0
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__channel.close()
+        self.__client.close()
+
+    def connect(self):
+        self.__client = paramiko.SSHClient()
+        self.__client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        if self.__password:
+            self.__client.connect(hostname=self.addr, username=self.user, password=self.__password, allow_agent=True)
+        else:
+            self.__client.connect(hostname=self.addr, username=self.user, allow_agent=True)
+
+        self.__channel = self.__client.get_transport().open_session()
+        self.__channel.get_pty()
+        self.__channel.setblocking(True)
+
+    def run_command(self, cmd: str) -> bool:
+        stdin, stdout, stderr = self.__client.exec_command(cmd)
+        out_lines = stdout.readlines()
+        for line in out_lines:
+            print(self.addr, ":", line.strip())
+        return len(out_lines) > 0
+
+    def run_command_with_password(self, cmd: str):
+        self.__channel.exec_command(cmd)
+
+        stdout = b''
+        while not self.__channel.recv_ready():
+            stdout = self.__channel.recv(4096)
+            if re.search('[Pp]assword', stdout.decode()):
+                self.__channel.send((self.__password + '\n').encode())
+            time.sleep(1)
+
+        while self.__channel.recv_ready():
+            stdout += self.__channel.recv(4096)
+        self.last_command_exit_status = self.__channel.exit_status
+        print(self.addr, stdout.decode())
+        return self.last_command_exit_status
+
+
+if __name__ == "__main__":
+    pass
+    #with SSH("egor@localhost", "123") as conn:
+    #    print(conn.run_command("uname -a"))
+        #conn.run_command_with_password("sudo cat /etc/sudoers")
---
+++ ssh/ssh_commands.py
@@ -0,0 +1,30 @@
+
+from utils.shell_commands import shell_run_command
+from ssh.SSH import SSH
+
+
+def run_ssh_command(host: str, cmd: str):
+    return shell_run_command(f"ssh -t {host} \"{cmd}\"")
+
+
+def copy_ssh_id(ssh_host: str):
+    return shell_run_command(f"ssh-copy-id {ssh_host}")
+
+
+def disable_password_asking(host_addr: str, password: str, sudoers_file: str):
+    cmd = f"echo \'%sudo ALL=(ALL) NOPASSWD:ALL\' | sudo tee {sudoers_file}"
+
+    with SSH(host_addr, password) as conn:
+        while conn.run_command_with_password(cmd):
+            if input("Try again? (Y/n): ").lower()[0] == "n":
+                return False
+    return True
+    #while run_ssh_command_with_password(host_addr, password, cmd):
+    #    if input("Try again? (Y/n): ").lower()[0] == "n":
+    #        return False
+    #    #password = getpass.getpass("Type password manually: ")
+    #return True
+
+
+if __name__ == "__main__":
+    pass
